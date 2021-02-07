edd6d3180d7d4573f3cd8821d3a82cdfd058441e
Jared Wasinger <j-wasinger@hotmail.com>
2021-02-07 21:06:48 +0000

    more wip

--- test-gen.js
+++ test-gen.js
@@ -1,8 +1,14 @@
+const {gen_return, gen_mstore, gen_mstore_multi, gen_addmod384, gen_submod384, gen_muldmod384} = require('./util.js')
+
 /*
 basic test does:
 1) emplace evm384 field parameters and test values in right memory locations
 2) execute evm384 opcode and return result
 */
+
+function calc_num_limbs(value) {
+    return Math.ceil(value.toString(16).length / 16)
+}
 
 function reverse_endianness(val) {
     if (val.length % 2 != 0) {
@@ -58,29 +64,50 @@ function encode_field_params(modulus, modinv) {
     return result
 }
 
+// encode a number as the little-endian representation that is 64bit * num_limbs long
 function encode_value(value, num_limbs) {
+    let val_str = value.toString(16)
+    let fill_len = (num_limbs * 16) - val_str.length
+    return reverse_endianness("0".repeat(fill_len) + val_str)
+}
 
+function gen_evm384_op(operation, offset_out, offset_x, offset_y, offset_field_params) {
+    let func = null
+
+    if (operation == 'addmod384') {
+        func = gen_addmod384
+    } else if (operation == 'submod384') {
+        func = gen_submod384
+    } else if (operation == 'muldmodmont384') {
+        func = gen_mulmodmont384
+    } else {
+        throw("operation must be mulmodmont384, addmod384 or submod384")
+    }
+
+    return func(offset_out, offset_x, offset_y, offset_field_params)
+}
+
+function gen_testcase(operation, x, y, modulus, modinv) {
+    const num_limbs = calc_num_limbs(modulus)
+    const offset_out = 0
+    const offset_x = offset_out + 8 * num_limbs
+    const offset_y = offset_x + 8 * num_limbs
+    const offset_field_params = offset_y + 8 * num_limbs
+
+    let ops = [
+        gen_mstore_multi(offset_field_params, encode_field_params(modulus, modinv)),
+        gen_mstore_multi(offset_x, encode_value(x, num_limbs)),
+        gen_mstore_multi(offset_y, encode_value(y, num_limbs)),
+        gen_mstore_multi(offset_out, encode_value(0, num_limbs)),
+        gen_evm384_op(operation, offset_out, offset_x, offset_y, offset_field_params),
+        gen_return(offset_out, num_limbs * 8)
+    ]
+
+    console.log(ops.join(""))
 }
 
 // modulus from bn128, modinv from bls12381
-let encoded = encode_field_params(21888242871839275222246405745257275088548364400416034343698204186575808495617n, 0x89f3fffcfffcfffdn)
-console.log(encoded)
+const bls12381_modulus = 0x1a0111ea397fe69a4b1ba7b6434bacd764774b84f38512bf6730d2a0f6b0f6241eabfffeb153ffffb9feffffffffaaabn
+const bls12381_modinv = 0x89f3fffcfffcfffdn
 
-function gen_testcase(operation, x, y, modulus, modinv) {
-    ops = []
-
-    const num_limbs = calc_num_limbs(modulus)
-    const out_offset = 0
-    const x_offset = out_offset + 8 * num_limbs
-    const y_offset = x_offset + 8 * num_limbs
-    const field_params_offset = y_offset + 8 * num_limbs
-
-    ops += [
-        gen_mstore_multi(field_params_offset, encode_field_params(modulus, modinv)),
-        gen_mstore_multi(offset_x, encode_value(offset_x, num_limbs)),
-        gen_mstore_multi(offset_y, encode_value(offset_y, num_limbs)),
-        gen_mstore_multi(offset_out, encode_value(0, num_limbs)),
-        gen_evm384_op(operation, offset_out, offset_x, offset_y, field_params_offset),
-        gen_return(offset_out, num_limbs * 8)
-    ]
-}
+gen_testcase("addmod384", 2n, 2n, bls12381_modulus, bls12381_modinv)
