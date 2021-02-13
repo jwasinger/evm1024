895da317459785a3905b2a914e2f550721bab5c2
Jared Wasinger <j-wasinger@hotmail.com>
2021-02-13 00:37:27 +0000

    tests output summary of num failed/passed

--- src/exec_tests.js
+++ src/exec_tests.js
@@ -8,7 +8,9 @@ const tempfile = require('tempfile')
 function exec_evmone(code_file) {
     return new Promise(resolve => {
         command = path.normalize("evmone/build/bin/evmone-bench --benchmark_format=json --benchmark_color=false " + code_file + " 00 01")
+/*
         console.log("executing\n" + command)
+*/
 
         exec(command, (a, b, sdf) => { 
             resolve(b.includes("iterations") && !b.includes("error_message")) })
@@ -17,10 +19,20 @@ function exec_evmone(code_file) {
 
 
 glob("tests/*.hex", null, async (err, files) => {
+    let failed = 0
+    let passed = 0
+
     for (let i = 0; i < files.length; i++) {
+
         let result = await exec_evmone(files[i])
         if (!result) {
-            throw("failed")
+            // console.log("failed")
+            failed++
+        } else {
+            passed++
         }
     }
+
+    console.log("passed: " + passed.toString())
+    console.log("failed: " + failed.toString())
 })
